6826417ec3e7beebd3c7743240824b6469d88fb0
Borna Butković <dev@bbutkovic.com>
2023-02-01 10:47:59 +0100

    cleanup

--- runtime/src/main.rs
+++ runtime/src/main.rs
@@ -1,4 +1,4 @@
-use std::{ffi::CString, sync::Mutex};
+use std::ffi::CString;
 
 use fastly::{Error, Request, Response as FastlyResponse};
 mod php;
@@ -6,53 +6,15 @@ mod response;
 
 use response::Response;
 
-const STREAMING: u32 = 1;
-
 #[fastly::main]
 fn main(req: Request) -> Result<FastlyResponse, Error> {
     Response::initialize();
 
     run_php(req, "phpinfo();");
 
-    // let res = Response::new();
-
-    // (*RUNTIME.lock().unwrap()).set_response(res);
-
-    // *RESPONSE.lock().unwrap() = Some(&mut Response::new());
-
-    // *RESPONSE.lock().unwrap() = Some();
-
-    // let res = Response::new();
-    // set_current_response(res);
-
-    // ResponseHandle::try_from(5);
-    // let mut res = Response::new();
-
-    // run_php(req, &mut res, "phpinfo();");
-
-    // // let body = OUTPUT.lock().unwrap().to_string();
-
-    // Ok(res)
-    // Ok(Response::from_body(body))
-
     let res = Response::get();
     Ok(res)
 }
-
-// pub fn init
-
-// fn embed_ub_write(
-//     _: i32,
-// ) -> unsafe extern "C" fn(str: *const ::std::os::raw::c_char, str_length: php::size_t) -> php::size_t
-// {
-//     |str: *const ::std::os::raw::c_char, str_length: php::size_t| -> php::size_t {
-//         let str = std::ffi::CStr::from_ptr(str).to_str().unwrap();
-
-//         *OUTPUT.lock().unwrap() += str;
-
-//         0
-//     }
-// }
 
 pub fn run_php(req: Request, code: &str) -> Option<String> {
     let code = CString::new(code).unwrap();
